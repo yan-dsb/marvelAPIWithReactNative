2402fd30e86169c813413dfe37f0ee39a2e366a2
Yan Borges <yandsb16@gmail.com>
2020-10-17 16:33:19 -0400

    renamed variable

--- src/persistence/Store.js
+++ src/persistence/Store.js
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 export const getData = async (key) => {
   try {
-    const favouritesStored = await AsyncStorage.getItem(`@${key}`);
-    return favouritesStored;
+    const data = await AsyncStorage.getItem(`@${key}`);
+    return data;
   } catch (e) {
     console.log(e);
   }
